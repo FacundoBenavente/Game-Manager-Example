eaaad48340b5044194594e6a53e09106f03b331f
FacundoBenavente <111788429+FacundoBenavente@users.noreply.github.com>
2025-08-19 10:34:38 -0300

    Cambios de Escena

---
+++ Assets/SceneManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public static SceneManagerScript Instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance !=null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Inicio");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Destino");
+        }
+    }
+}
